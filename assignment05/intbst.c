968dd9c2d1816b5324de3536d68ad7fc46162bed
LDevine56 <sd081104@gmail.com>
2022-05-16 02:12:28 +0900

    build: (4)

--- assignment05/intbst.c
+++ assignment05/intbst.c
@@ -3,21 +3,21 @@
 #include <assert.h>
 #include <time.h> // time
 
-#define RANDOM_INPUT	1
-#define FILE_INPUT		2
+#define RANDOM_INPUT 1
+#define FILE_INPUT 2
 
 ////////////////////////////////////////////////////////////////////////////////
 // TREE type definition
 typedef struct node
 {
-	int			data;
-	struct node	*left;
-	struct node	*right;
+	int data;
+	struct node *left;
+	struct node *right;
 } NODE;
 
 typedef struct
 {
-	NODE	*root;
+	NODE *root;
 } TREE;
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -27,97 +27,98 @@ typedef struct
 	return	head node pointer
 			NULL if overflow
 */
-TREE *BST_Create( void);
+TREE *BST_Create(void);
 
 /* Deletes all data in tree and recycles memory
-*/
-void BST_Destroy( TREE *pTree);
+ */
+void BST_Destroy(TREE *pTree);
 
 /* internal function (not mandatory)
-*/
-static void _destroy( NODE *root);
+ */
+static void _destroy(NODE *root);
 
 /* Inserts new data into the tree
 	return	1 success
 			0 overflow
 */
-int BST_Insert( TREE *pTree, int data);
+int BST_Insert(TREE *pTree, int data);
 
 /* internal function (not mandatory)
-*/
-static void _insert( NODE *root, NODE *newPtr);
+ */
+static void _insert(NODE *root, NODE *newPtr);
 
-NODE *_makeNode( int data);
+NODE *_makeNode(int data);
 
 /* Deletes a node with dltKey from the tree
 	return	1 success
 			0 not found
 */
-int BST_Delete( TREE *pTree, int dltKey);
+int BST_Delete(TREE *pTree, int dltKey);
 
 /* internal function
 	success is 1 if deleted; 0 if not
 	return	pointer to root
 */
-static NODE *_delete( NODE *root, int dltKey, int *success);
+static NODE *_delete(NODE *root, int dltKey, int *success);
 
 /* Retrieve tree for the node containing the requested key
 	return	address of data of the node containing the key
 			NULL not found
 */
-int *BST_Retrieve( TREE *pTree, int key);
+int *BST_Retrieve(TREE *pTree, int key);
 
 /* internal function
 	Retrieve node containing the requested key
 	return	address of the node containing the key
 			NULL not found
 */
-static NODE *_retrieve( NODE *root, int key);
+static NODE *_retrieve(NODE *root, int key);
 
 /* prints tree using inorder traversal
-*/
-void BST_Traverse( TREE *pTree);
-static void _traverse( NODE *root);
+ */
+void BST_Traverse(TREE *pTree);
+static void _traverse(NODE *root);
 
 /* Print tree using inorder right-to-left traversal
-*/
-void printTree( TREE *pTree);
+ */
+void printTree(TREE *pTree);
 /* internal traversal function
-*/
-static void _inorder_print( NODE *root, int level);
+ */
+static void _inorder_print(NODE *root, int level);
 
-/* 
+/*
 	return 1 if the tree is empty; 0 if not
 */
-int BST_Empty( TREE *pTree);
+int BST_Empty(TREE *pTree);
 
 ////////////////////////////////////////////////////////////////////////////////
-int main( int argc, char **argv)
+int main(int argc, char **argv)
 {
 	int mode; // input mode
 	TREE *tree;
 	int data;
-	
+
 	if (argc != 2)
 	{
-		fprintf( stderr, "usage: %s FILE or %s number\n", argv[0], argv[0]);
+		fprintf(stderr, "usage: %s FILE or %s number\n", argv[0], argv[0]);
 		return 1;
 	}
-	
+
 	FILE *fp;
-	
+
 	if ((fp = fopen(argv[1], "rt")) == NULL)
 	{
 		mode = RANDOM_INPUT;
 	}
-	else mode = FILE_INPUT;
-	
+	else
+		mode = FILE_INPUT;
+
 	// creates a null tree
 	tree = BST_Create();
-	
+
 	if (!tree)
 	{
-		printf( "Cannot create a tree!\n");
+		printf("Cannot create a tree!\n");
 		return 100;
 	}
 
@@ -125,80 +126,83 @@ int main( int argc, char **argv)
 	{
 		int numbers;
 		numbers = atoi(argv[1]);
-		assert( numbers > 0);
+		assert(numbers > 0);
 
-		fprintf( stdout, "Inserting: ");
-		
-		srand( time(NULL));
+		fprintf(stdout, "Inserting: ");
+
+		srand(time(NULL));
 		for (int i = 0; i < numbers; i++)
 		{
-			data = rand() % (numbers*3) + 1; // random number (1 ~ numbers * 3)
-			
-			fprintf( stdout, "%d ", data);
-			
+			data = rand() % (numbers * 3) + 1; // random number (1 ~ numbers * 3)
+
+			fprintf(stdout, "%d ", data);
+
 			// insert function call
-			int ret = BST_Insert( tree, data);
-			if (!ret) break;
+			int ret = BST_Insert(tree, data);
+			if (!ret)
+				break;
 		}
 	}
 	else if (mode == FILE_INPUT)
 	{
-		fprintf( stdout, "Inserting: ");
-		
-		while (fscanf( fp, "%d", &data) != EOF)
-		{
-			fprintf( stdout, "%d ", data);
-			
-			// insert function call
-			int ret = BST_Insert( tree, data);
-			if (!ret) break;
-		}
-		fclose( fp);
-	}
-	
-	fprintf( stdout, "\n");
+		fprintf(stdout, "Inserting: ");
 
-	if (BST_Empty( tree))
+		while (fscanf(fp, "%d", &data) != EOF)
+		{
+			fprintf(stdout, "%d ", data);
+
+			// insert function call
+			int ret = BST_Insert(tree, data);
+			if (!ret)
+				break;
+		}
+		fclose(fp);
+	}
+
+	fprintf(stdout, "\n");
+
+	if (BST_Empty(tree))
 	{
-		fprintf( stdout, "Empty tree!\n");
-		BST_Destroy( tree);
+		fprintf(stdout, "Empty tree!\n");
+		BST_Destroy(tree);
 		return 0;
-	}	
+	}
 
 	// inorder traversal
-	fprintf( stdout, "Inorder traversal: ");
-	BST_Traverse( tree);
-	fprintf( stdout, "\n");
-	
+	fprintf(stdout, "Inorder traversal: ");
+	BST_Traverse(tree);
+	fprintf(stdout, "\n");
+
 	// print tree with right-to-left inorder traversal
-	fprintf( stdout, "Tree representation:\n");
+	fprintf(stdout, "Tree representation:\n");
 	printTree(tree);
-	
+
 	while (1)
 	{
-		fprintf( stdout, "Input a number to delete: "); 
+		fprintf(stdout, "Input a number to delete: ");
 		int num;
-		if (scanf( "%d", &num) == EOF) break;
-		
-		int ret = BST_Delete( tree, num);
+		if (scanf("%d", &num) == EOF)
+			break;
+
+		int ret = BST_Delete(tree, num);
 		if (!ret)
 		{
-			fprintf( stdout, "%d not found\n", num);
+			fprintf(stdout, "%d not found\n", num);
 			continue;
 		}
-		
+
 		// print tree with right-to-left inorder traversal
-		fprintf( stdout, "Tree representation:\n");
+		fprintf(stdout, "Tree representation:\n");
 		printTree(tree);
-		
-		if (BST_Empty( tree))
+
+		if (BST_Empty(tree))
 		{
-			fprintf( stdout, "Empty tree!\n");
+			fprintf(stdout, "Empty tree!\n");
 			break;
 		}
 	}
-	
-	BST_Destroy( tree);
+
+	BST_Destroy(tree);
 
 	return 0;
 }
@@ -210,14 +214,14 @@ int main( int argc, char **argv)
 TREE *BST_Create(void)
 {
 	TREE *newTree = (TREE *)malloc(sizeof(TREE));
-	NODE *newNode = (NODE *)malloc(sizeof(NODE));
-	
+	// NODE* newNode = (NODE*)malloc(sizeof(NODE));
+	// newNode -> data
 
-	if (newTree == NULL || newNode == NULL )
+	if (newTree == NULL)
 	{
 		return NULL;
 	}
-	newTree->root = newNode;
+	newTree->root = NULL;
 	return newTree;
 }
 
@@ -253,63 +257,52 @@ int BST_Insert(TREE *pTree, int data)
 	NODE *newPtr = _makeNode(data);
 	if (newPtr == NULL)
 		return 0;
+
+	if (pTree->root == NULL)
+	{
+		pTree->root = newPtr;
+		return 1;
+	}
 	_insert(pTree->root, newPtr);
 	return 1;
 }
 
 /* internal function (not mandatory)
  */
-static void _insert(NODE *root, NODE *newPtr)
+static void _insert(NODE *root, NODE *newPtr) // root에 NULL값이 들어오면 위치 알려줄 수 없음. 함수의 parameter는 함수의 stack에 저장됨.
 {
-	NODE* current = (NODE *)malloc(sizeof(NODE));
-	NODE* parent = (NODE *)malloc(sizeof(NODE));
-	if(current == NULL || parent == NULL){
-		return ;
-	}
+	NODE *p;
+	NODE *t;
 
-	parent = root;
+	p = root;
 
-	if(parent == NULL){
-		root = newPtr;
-	}
-
-	while(parent != NULL){
-		current = parent;
-		if(newPtr->data < parent->data){
-			parent = parent -> left;
-		} else{
-			parent = parent->right;
+	while (p != NULL)
+	{
+		if (newPtr->data < p->data)
+		{
+			t = p;
+			p = p->left;
+		}
+		else
+		{
+			t = p;
+			p = p->right;
 		}
 	}
 
-	if(newPtr -> data < current -> data){
-		current -> left = newPtr; 
-	} else{
-		current->right = newPtr;
-	}
-	free(current);
-	free(parent);
-
-	// if (!root)
-	// {
-	// 	root = newPtr;
-	// }
-
-	// if (newPtr->data < root->data)
-	// {
-	// 	return _insert(root->left, newPtr);
-	// }
-	// else
-	// { // newPtr->data >= root->data
-	// 	return _insert(root->right, newPtr);
-	// }
+	if (newPtr->data < t->data)
+		t->left = newPtr;
+	else
+		t->right = newPtr;
+	return;
 }
 
 NODE *_makeNode(int data)
 {
 	NODE *new = (NODE *)malloc(sizeof(NODE));
-	if(new == NULL){
-		return 0;
+	if (new == NULL)
+	{
+		return NULL;
 	}
 	new->data = data;
 	new->left = NULL;
@@ -373,19 +366,18 @@ static NODE *_delete(NODE *root, int dltKey, int *success)
 // BST_Retrieve를 insert와 delete에 사용?
 int *BST_Retrieve(TREE *pTree, int key)
 {
-	NODE* find = (NODE *)malloc(sizeof(NODE));
-	if(find == NULL){
+	NODE *find = (NODE *)malloc(sizeof(NODE));
+	if (find == NULL)
+	{
 		return 0;
 	}
-	int pData;
 
-	if(pTree != NULL)
-		find = _retrieve(pTree -> root, key);
-	if(find == NULL)
+	if (pTree != NULL)
+		find = _retrieve(pTree->root, key);
+	if (find == NULL)
 		return NULL;
-	pData = find-> data;
-	free(find);
-	return &(pData); // local 변수 반환 X. 포인터 찾자
+
+	return &(find->data); // local 변수 반환 X. 포인터 찾자
 }
 
 /* internal function
@@ -412,16 +404,20 @@ static NODE *_retrieve(NODE *root, int key)
  */
 void BST_Traverse(TREE *pTree)
 {
+	if (pTree->root == NULL)
+	{
+		return;
+	}
 	_traverse(pTree->root);
 }
 static void _traverse(NODE *root)
 {
-	if (!root->left)
+	if (root->left != NULL)
 		_traverse(root->left);
 
 	printf("%d ", root->data);
 
-	if (!root->right)
+	if (root->right != NULL)
 		_traverse(root->right);
 }
 
@@ -430,16 +426,19 @@ static void _traverse(NODE *root)
 void printTree(TREE *pTree)
 {
 	int level = 0;
+	if (pTree->root == NULL)
+	{
+		return;
+	}
 	_inorder_print(pTree->root, level);
 }
 /* internal traversal function
  */
 static void _inorder_print(NODE *root, int level)
 {
-	if (!root->right)
+	if (root->right != NULL)
 	{
-		level++;
-		_inorder_print(root->right, level);
+		_inorder_print(root->right, level + 1);
 	}
 
 	for (int i = 0; i < level; i++)
@@ -448,10 +447,9 @@ static void _inorder_print(NODE *root, int level)
 	}
 	printf("%d \n", root->data);
 
-	if (!root->left)
+	if (root->left != NULL)
 	{
-		level++;
-		_inorder_print(root->left, level);
+		_inorder_print(root->left, level + 1);
 	}
 }
 
@@ -460,7 +458,7 @@ static void _inorder_print(NODE *root, int level)
 */
 int BST_Empty(TREE *pTree)
 {
-	if (!pTree->root)
+	if (pTree->root == NULL)
 		return 1;
 	return 0;
 }
